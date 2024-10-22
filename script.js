5519284871779a2e1b83c730f8a8925bc4fff619
high <156235280+highdeveloperr@users.noreply.github.com>
2024-10-22 13:51:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const validCredentials = {
     admin: { password: 'admin123', redirect: 'protected' },
     user1: { password: 'user1', redirect: 'user1.html' },
-    MOVIE: { password: 'MINION', redirect: 'user2.html' },
+    minion: { password: 'minion', redirect: 'user2.html' },
 };
 
 document.getElementById('loginForm').addEventListener('submit', function(event) {
